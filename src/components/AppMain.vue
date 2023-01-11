7065d8d33ec0ed694752507b08e4962dc3edde7f
MonicaDeBona <debona.monica@outlook.it>
2023-01-11 17:09:44 +0100

    add axios e api

--- src/components/AppMain.vue
+++ src/components/AppMain.vue
@@ -1,6 +1,32 @@
 <script >
+    import { store } from '../store.js';
+    import axios from 'axios';
+
     export default {
         name: 'AppMain',
+        data() {
+            return {
+                store,
+                apiCardList: ' https://db.ygoprodeck.com/api/v7/cardinfo.php?num=10&offset=0'
+            }
+        },
+        methods: {
+            getCardElement() {
+                //dentro recupero tramite ajax le carte
+                axios.get(this.apiCardList, {
+                    params: {
+
+                    }
+                })
+                .then((response) => {
+                    this.store.cardsList = response.data.data;
+                })
+            }
+        },
+
+        created() {
+            this.getCardElement();
+        }
     }
 </script>
 
